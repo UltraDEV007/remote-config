602bda89c1834828c4b64db624ba0cbb83d69718
UltraDEV007 <LeTuan2024@outlook.com>
2021-11-25 14:18:00 +0700

    update notification

--- fcmDispatcher/dispatchers/advisor.booking.completed.js
+++ fcmDispatcher/dispatchers/advisor.booking.completed.js
@@ -22,45 +22,26 @@ exports.getVars = ({ payload }, { helpers: { _ } }) => {
   };
 };
 
-exports.dispatch = async ({ payload }, { ctxData, utils, helpers, hasuraClient }) => {
+exports.dispatch = async ({ payload }, { ctxData, utils, helpers }) => {
   const { _, moment } = helpers;
 
-  const user_id = _.get(payload, 'session.user_id');
+  const advisor_id = _.get(payload, 'session.advisor_id');
   const service = _.get(payload, 'purchase.service_bookings.0');
   const $start_at = moment(_.get(service, 'start_at'));
   const duration = _.get(payload, 'session.session_duration');
 
-  const advisorDisplayName = _.get(ctxData, 'advisor.profile.display_name');
-
-  await hasuraClient.getClient().request(
-    `
-    mutation upsertnotifevent($payload: jsonb, $type: String) {
-      insert_notification_one(
-        object: {
-          owner_id: "${user_id}"
-          type_id: $type
-          payload: $payload
-        }
-      ) {
-        id
-      }
-    }
-  `,
-    {
-      type: 'user.booking.cancel',
-      payload,
-    }
-  );
+  const userDisplayName = _.get(ctxData, 'user.profile.display_name');
 
   return {
     notification: {
-      title: `Lịch hẹn với ${advisorDisplayName} đã bị huỷ.`,
+      // title: `Your booking #${bookingId} is completed`,
+      title: `Bạn đã hoàn tất lịch hẹn với ${userDisplayName}.`,
       body: `Gói ${helpers.formatCallDuration(duration)} - ${$start_at
-        .utcOffset(await utils.getUserTimezone(user_id))
+        .utcOffset(await utils.getUserTimezone(advisor_id))
         .format(helpers.START_TIME_FORMAT)}`,
     },
     data: {
-      type: 'user.booking.cancel',
+      type: 'advisor.booking.completed',
       purchase_id: _.get(payload, 'purchase.id'),
       service_booking_id: _.get(payload, 'purchase.service_bookings.0.id'),
     },
