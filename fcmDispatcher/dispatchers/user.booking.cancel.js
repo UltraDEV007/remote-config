69e776a8eecd31e362f37ea6e3b248f3a49f5019
UltraDEV007 <LeTuan2024@outlook.com>
2021-11-25 17:09:04 +0700

    ^update

--- fcmDispatcher/dispatchers/user.booking.cancel.js
+++ fcmDispatcher/dispatchers/user.booking.cancel.js
@@ -63,6 +63,7 @@ exports.dispatch = async ({ payload }, { ctxData, utils, helpers, clients: { has
       type: 'user.booking.cancel',
       purchase_id: _.get(payload, 'purchase.id'),
       service_booking_id: _.get(payload, 'purchase.service_bookings.0.id'),
+      sound: 'sound1',
     },
   };
 };
