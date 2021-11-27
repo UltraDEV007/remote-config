896de86fb535d4d76374d4f8d6ea1ad78045f2bc
UltraDEV007 <LeTuan2024@outlook.com>
2021-11-27 17:37:01 +0700

    advisor.room.connect

---
+++ fcmDispatcher/dispatchers/advisor.room.connect.js
@@ -0,0 +1,75 @@
+exports.getQuery = () => `
+  query($advisor_id: String!, $room_id: uuid!, $course_id: uuid!) {
+    advisor: advisor_by_pk(id: $advisor_id) {
+      id
+      profile {
+        display_name
+      }
+    }
+    room: course_room_by_pk(id: $room_id) {
+      id
+      start_at
+      end_at
+    }
+    course: course_by_pk(id: $course_id) {
+      id
+      name
+    }
+  }
+`;
+
+exports.getVars = ({ payload }, { helpers: { _ } }) => {
+  return {
+    advisor_id: _.get(payload, 'course.advisor_id'),
+    course_id: _.get(payload, 'course.id'),
+    room_id: _.get(payload, 'room.id'),
+  };
+};
+
+exports.dispatch = async ({ payload }, { ctxData, helpers }) => {
+  const { _, moment } = helpers;
+
+  const course = _.get(ctxData, 'course');
+  const room = _.get(ctxData, 'room');
+  const start_at = _.get(ctxData, 'room.start_at');
+  const end_at = _.get(ctxData, 'room.start_at');
+  const duration = moment(end_at).diff(moment(start_at), 'second');
+
+  const courseDisplayName = _.get(course, 'name');
+
+  const title = `Lớp học ${courseDisplayName}`;
+  const body = `Đã bắt đầu - Thời lượng ${helpers.formatCallDuration(duration)} phút.`;
+
+  return {
+    notification: {
+      title,
+      body,
+    },
+    data: {
+      type: 'advisor.room.connect',
+      room_id: _.get(room, 'id') || '',
+      course_id: _.get(course, 'id') || '',
+      sound: 'sound1',
+    },
+    apns: {
+      payload: {
+        aps: {
+          alert: {
+            title,
+            body,
+          },
+          sound: 'notification.mp3',
+        },
+      },
+    },
+    android: {
+      priority: 'high',
+      data: {
+        sound: 'notification',
+      },
+      notification: {
+        sound: 'notification',
+      },
+    },
+  };
+};
