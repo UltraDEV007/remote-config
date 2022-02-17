e1ec13f01646e34e79612b3e611c3360e670d69a
UltraDEV007 <LeTuan2024@outlook.com>
2022-02-17 09:42:37 +0700

    Update room.js

--- workflow/templates/room.js
+++ workflow/templates/room.js
@@ -1,6 +1,6 @@
 const config = {
-  remind_room_before_start_at: '5 minutes',
-  connect_room_before_start_at: '5 seconds',
+  remind_room_before_start_at: '15 minutes',
+  connect_room_before_start_at: '2 minutes',
   complete_room_after_end_at: '1 minutes',
 };
 
