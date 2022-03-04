dc52e4c4d5d43adb5f4ab4b997d4e24cfcf33f06
UltraDEV007 <LeTuan2024@outlook.com>
2022-03-04 15:55:05 +0700

    Update course.js

--- workflow/templates/course.js
+++ workflow/templates/course.js
@@ -1,7 +1,7 @@
 const config = {
   start_room_before_start_at: '30 minutes',
   postpone_course_before_start_at: '10 minutes',
-  postpone_course_update_after: '30 minutes',
+  postpone_course_update_after: '1020 minutes',
   onprogress_course_after_at: '1 minutes',
 };
 
