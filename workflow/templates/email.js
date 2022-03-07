4f5db876a6e4405b3fb456cdfd69e9ae76caedd3
UltraDEV007 <LeTuan2024@outlook.com>
2022-03-07 16:39:09 +0700

    ^email config

---
+++ workflow/templates/email.js
@@ -0,0 +1,33 @@
+const config = [
+  {
+    title: 'Unitz community',
+    name: 'UNITZ COMMUNITY 1',
+    url: 'https://www.facebook.com/groups/unitz.community',
+  },
+  {
+    title: 'Fanpage',
+    name: 'UNITZ APP 1',
+    url: 'https://www.facebook.com/UnitzApp',
+  },
+  {
+    title: 'Zalo',
+    name: 'Công Ty TNHH UNITZ 1',
+    url: 'https://zalo.me/2510053737522180594',
+  },
+  {
+    title: 'Email',
+    name: 'support@unitz.app 1',
+    url: 'mailto:support@unitz.app',
+  },
+  {
+    title: 'Website',
+    name: 'https://unitz.app 1',
+    url: 'https://unitz.app',
+  },
+  {
+    title: 'Phone number',
+    name: '0909663807 (Ms. My)',
+  },
+];
+
+exports.config = config;
