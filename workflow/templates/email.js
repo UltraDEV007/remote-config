7b101ca6ce755d6d992c8b70a93fe4bf3ef41828
UltraDEV007 <LeTuan2024@outlook.com>
2022-03-07 16:54:27 +0700

    ^ email contact form

---
+++ workflow/templates/email.js
@@ -0,0 +1,33 @@
+const config = [
+  {
+    title: 'Unitz community',
+    name: 'UNITZ COMMUNITY',
+    url: 'https://www.facebook.com/groups/unitz.community',
+  },
+  {
+    title: 'Fanpage',
+    name: 'UNITZ APP',
+    url: 'https://www.facebook.com/UnitzApp',
+  },
+  {
+    title: 'Zalo',
+    name: 'Công Ty TNHH UNITZ',
+    url: 'https://zalo.me/2510053737522180594',
+  },
+  {
+    title: 'Email',
+    name: 'support@unitz.app',
+    url: 'mailto:support@unitz.app',
+  },
+  {
+    title: 'Website',
+    name: 'https://unitz.app',
+    url: 'https://unitz.app',
+  },
+  {
+    title: 'Phone number',
+    name: '0909663807 (Ms. My)',
+  },
+];
+
+exports.config = config;
