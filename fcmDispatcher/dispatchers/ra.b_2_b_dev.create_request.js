69e7ce3ad117dcaf96f743b502672ea870e93b23
UltraDEV007 <LeTuan2024@outlook.com>
2022-12-10 22:17:07 +0700

    ra

--- fcmDispatcher/dispatchers/ra.b_2_b_dev.create_request.js
+++ fcmDispatcher/dispatchers/ra.b_2_b_dev.create_request.js
@@ -1,7 +1,21 @@
 exports.getQuery = () => `
-  query($ar_id: uuid!) {
+  query($ar_id: uuid!, $user_id: String!, $account_id: uuid!) {
     ar: b2b_approval_request_by_pk(id: $ar_id) {
       id
+    }
+    user: user_by_pk(id: $user_id) {
+      id
+      profile {
+        display_name
+      }
+    }
+    account: b2b_account_by_pk(id: $account_id) {
+      id
+      slug
+      profile: account_profile {
+        display_name
+        avatar_url
+      }
     }
   }
 `;
@@ -9,6 +23,7 @@ exports.getQuery = () => `
 exports.getVars = ({ payload }, { helpers: { _ } }) => {
   return {
     ar_id: _.get(payload, 'id'),
+    user_id: _.get(payload, 'item.created_by'),
   };
 };
 
@@ -22,14 +37,19 @@ exports.dispatch = async ({ payload }, { ctxData, helpers, utils, clients: { rou
 exports.effect = async ({ payload }, { ctxData, helpers, utils, clients }) => {
   const { _, moment } = helpers;
 
-  // const account = _.get(ctxData, 'account');
+  const account = _.get(ctxData, 'account');
 
-  // const user_id = _.get(ctxData, 'user.id');
-  // const i18n = await utils.forUser(user_id);
+  const user_id = _.get(ctxData, 'user.id');
+  const i18n = await utils.forUser(user_id);
 
   // slack message effect
   clients.slackClient.getClient().sendMessage({
     ...payload,
+    ...ctxData,
+    route: {
+      user_url: clients.routeWebClient.getClient().toToolUrl('profile'),
+      account_url: clients.routeWebClient.getClient().toToolUrl('toolAccountDetail', account),
+    },
   });
   // send email effect
   // clients.sendgridClient.getClient().sendEmail(user_id, {
