eaf300d1c52ca07329c27da45470515a5922b5d8
UltraDEV007 <LeTuan2024@outlook.com>
2022-12-11 02:59:44 +0700

    routeWeb

--- routeWeb/app/route.js
+++ routeWeb/app/route.js
@@ -7,6 +7,128 @@ const routeRules = {
       return `/room?id=${_.get(room, 'id')}`;
     },
   },
+  course: {
+    url: (course) => {
+      if (_.get(course, 'id')) {
+        return `/course?id=${_.get(course, 'id')}`;
+      }
+      return '/course';
+    },
+  },
+  advisor: {
+    url: (advisor) => {
+      const hash = _.get(advisor, 'tab');
+      return `/advisor?id=${_.get(advisor, 'id')}${hash ? `#${hash}` : ''}`;
+    },
+  },
+  courseDetail: {
+    url: (course) => {
+      const hash = _.get(course, 'tab');
+      return `/course/detail?id=${_.get(course, 'id')}${hash ? `#${hash}` : ''}`;
+    },
+  },
+  calendar: {
+    url: () => {
+      return '/calendar';
+    },
+  },
+
+  addCourse: {
+    url: () => {
+      return '/course/add';
+    },
+  },
+
+  wallet: {
+    url: () => {
+      return '/wallet';
+    },
+  },
+
+  home: {
+    url: () => {
+      return '/';
+    },
+  },
+
+  courseFilter: {
+    url: () => {
+      return '/education/course-filter';
+    },
+  },
+
+  profile: {
+    url: () => {
+      return '/profile';
+    },
+  },
+
+  userWallet: {
+    url: () => {
+      return '/me/wallet';
+    },
+  },
+  payment: {
+    url: (course) => {
+      return `/course/purchase?id=${_.get(course, 'id')}`;
+    },
+  },
+
+  admin: {
+    advisor: {
+      url: (advisor) => {
+        return `/advisor/profile?id=${_.get(advisor, 'id')}`;
+      },
+      name: (advisor) => {
+        return _.get(advisor, 'profile.display_name');
+      },
+    },
+    course: {
+      url: (course) => {
+        return `/advisor/course?id=${_.get(course, 'id')}`;
+      },
+      name: (course) => {
+        return _.get(course, 'name');
+      },
+    },
+    room: {
+      url: (room) => {
+        return `/advisor/room?id=${_.get(room, 'id')}`;
+      },
+      name: (room) => {
+        return _.get(room, 'start_at');
+      },
+    },
+    user: {
+      url: (user) => {
+        return `/user/profile?id=${_.get(user, 'id')}`;
+      },
+      name: (advisor) => {
+        return _.get(advisor, 'profile.display_name');
+      },
+    },
+
+    rcms: {
+      url: (user) => {
+        return `/support/rcm?id=${_.get(user, 'ticket_id')}`;
+      },
+      name: (advisor) => {
+        return _.get(advisor, 'profile.display_name');
+      },
+    },
+  },
+
+  messageWithAdvisor: {
+    url: (advisor) => {
+      return `/me/message?id=${_.get(advisor, 'id')}`;
+    },
+  },
+
+  messageWithUser: {
+    url: (user) => {
+      return `/message?id=${_.get(user, 'id')}`;
+    },
+  },
 };
 
 _.map(routeRules, (rule, ruleName) => {
