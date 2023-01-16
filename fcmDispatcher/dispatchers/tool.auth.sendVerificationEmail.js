23b14d0c54e3d254355af66dcf9ba7d53f910b1f
UltraDEV007 <LeTuan2024@outlook.com>
2023-01-16 13:50:15 +0700

    verificationEmail

--- fcmDispatcher/dispatchers/tool.auth.sendVerificationEmail.js
+++ fcmDispatcher/dispatchers/tool.auth.sendVerificationEmail.js
@@ -29,22 +29,27 @@ exports.effect = async ({ payload }, { ctxData, helpers, utils, clients }) => {
   const user_id = _.get(ctxData, 'user.id');
   const i18n = await utils.forUser(user_id);
 
-
   // send email effect
-  clients.sendgridClient.getClient().sendEmail(user_id, {
-    template: {
-      name: i18n.getTemplateSuffixName('tool.auth.sendVerificationEmail'),
-    },
-    ...ctxData,
-    subject: 'Verify your email for UnitzApp',
-    admin_name: _.get(user, 'profile.display_name'),
-    login: {
-      link: clients.routeWebClient.getClient().toToolUrl('toolAccountDetail', account),
-    },
-    route: {
-      user_url: clients.routeWebClient.getClient().toToolUrl('profile'),
-      verify_email_url: _.get(payload, 'link.verifyEmail'),
-      unsubscribe_url: '/',
-    },
-  });
+  try {
+    const emailPayload = {
+      template: {
+        name: i18n.getTemplateSuffixName('tool.auth.sendVerificationEmail'),
+      },
+      ...ctxData,
+      subject: 'Verify your email for UnitzApp',
+      admin_name: _.get(user, 'profile.display_name'),
+      login: {
+        link: clients.routeWebClient.getClient().toToolUrl('toolAccountDetail', account),
+      },
+      route: {
+        user_url: clients.routeWebClient.getClient().toToolUrl('profile'),
+        verify_email_url: _.get(payload, 'link.verifyEmail'),
+        unsubscribe_url: '/',
+      },
+    };
+    console.log('emailPayload', emailPayload);
+    await clients.sendgridClient.getClient().sendEmail(user_id, emailPayload);
+  } catch (err) {
+    console.log('sendmail error', err);
+  }
 };
