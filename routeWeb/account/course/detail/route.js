9043ff78149958c772b65127d7090f91c572a121
UltraDEV007 <LeTuan2024@outlook.com>
2023-02-08 16:45:40 +0700

    add toolAccountCourseDetail route rule

---
+++ routeWeb/account/course/detail/route.js
@@ -0,0 +1,30 @@
+const _ = require('lodash');
+const { routeStore } = require('@vl/mod-utils/adminRouteStore');
+
+routeStore.addRule('toolAccountCourseDetail', {
+  url: (course) => {
+    const { account } = course;
+    let accountId = _.get(account, 'id');
+    let slug = _.get(account, 'slug');
+    let queryString = '';
+    if (slug) {
+      queryString = routeStore.queryString({ id: _.get(course, 'id') });
+      return `/${slug}/course${queryString}`;
+    }
+    if (accountId) {
+      queryString = routeStore.queryString({ accountId, id: _.get(course, 'id') });
+      return `/account/course${queryString}`;
+    }
+    return '/account/course';
+  },
+  parse: (urlObject) => {
+    const params = {};
+    for (let param in urlObject.searchParams) {
+      params[param] = urlObject.searchParams.get(param);
+    }
+    return params;
+  },
+  match: (urlObject) => {
+    return urlObject.pathname === 'account/course';
+  },
+});
